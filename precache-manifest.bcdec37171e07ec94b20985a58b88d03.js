a4cf7a8d2c87bdf41522801ceb2b3ba535292d14
Damilola Ajiboye <stephenajiboye7@gmail.com>
2019-02-19 08:21:38 +0100

    Updates

---
+++ precache-manifest.bcdec37171e07ec94b20985a58b88d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d46a7a90341e0ee405e",
+    "url": "/static/css/main.aede7b88.chunk.css"
+  },
+  {
+    "revision": "3d46a7a90341e0ee405e",
+    "url": "/static/js/main.3d46a7a9.chunk.js"
+  },
+  {
+    "revision": "babd94826ba891d51a02",
+    "url": "/static/js/1.babd9482.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ef3bb66d9216fffcea9022628f7bb26",
+    "url": "/static/media/weather.0ef3bb66.gif"
+  },
+  {
+    "revision": "587e0e2f626dc1a8e4c6990fc6af93f9",
+    "url": "/index.html"
+  }
+];
